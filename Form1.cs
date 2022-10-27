1dba9c54fe66f2fa3f4315c0d0587f5c91feece4
TriciaDPC <116187302+TriciaDPC@users.noreply.github.com>
2022-10-27 15:38:07 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class FrmBasicThread : Form
+    {
+        public Thread ThreadA, ThreadB;
+
+
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before Starting thread-");
+            ThreadStart threadssss = new ThreadStart(MyThreadClass.Thread1);
+
+            ThreadA = new Thread(threadssss);
+            ThreadA.Name = "Thread A Process";
+
+            ThreadB = new Thread(threadssss);
+            ThreadB.Name = "Thread B Process";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("-End of thread-");
+            label1.Text = ("-End of thread-");
+        }
+
+       
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int LoopCount = 0; LoopCount < 6; LoopCount++)
+            {       
+                Thread.Sleep(1500);
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + LoopCount);
+
+            }
+        }    
+    }
+}
